79cd91d578854354e0d48ee63073ab4d1c390b30
andrea-everett <andrea.dawn.everett@gmail.com>
2022-04-10 14:52:20 -0400

    final-touches

--- Documents/Documents - Joshua’s MacBook Air/coding-projects/side-projects/Article-Preview/index.js
+++ Documents/Documents - Joshua’s MacBook Air/coding-projects/side-projects/Article-Preview/index.js
@@ -1,11 +1,12 @@
 const shareButton = document.getElementById("share-button");
+    
 const active = document.getElementById("active");
 const shareButton2  = document.getElementById("share-button2");
 
 shareButton.addEventListener("click", () => {
-    active.classList.toggle("active");
+    active.classList.toggle("active-state");
 })
 
 shareButton2.addEventListener("click", () => {
-    active.classList.toggle("active");
+    active.classList.toggle("active-state");
 });
