aac4f6c986697b0e499b3bbfbdcd198ac1b7788c
andrea-everett <andrea.dawn.everett@gmail.com>
2022-04-10 11:26:24 -0400

    mobile-desktop edits

--- Documents/Documents - Joshua’s MacBook Air/coding-projects/side-projects/Article-Preview/index.js
+++ Documents/Documents - Joshua’s MacBook Air/coding-projects/side-projects/Article-Preview/index.js
@@ -1,8 +1,11 @@
-const shareDropDownButton = document.querySelector('.icon');
-const bottomIcons = document.querySelector('.topnav-right');
+const shareButton = document.getElementById("share-button");
+const active = document.getElementById("active");
+const shareButton2  = document.getElementById("share-button2");
 
-shareDropDownButton.addEventListener("click", () => {
-    console.log("click")
-    bottomIcons.classList.remove('topnav-right')
-    bottomIcons.classList.add('active-topnav')
+shareButton.addEventListener("click", () => {
+    active.classList.toggle("active");
 })
+
+shareButton2.addEventListener("click", () => {
+    active.classList.toggle("active");
+});
