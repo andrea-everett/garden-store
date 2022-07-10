4972b6690bf30a6929db1aa558208edb059c7568
andrea-everett <andrea.dawn.everett@gmail.com>
2022-07-10 16:22:28 -0400

    first-commit

---
+++ Documents/Documents - Joshua’s MacBook Air/coding-projects/all-projects/main.js
@@ -0,0 +1,18 @@
+function openWindow() {
+    window.open("https://andrea-everett.github.io/infinity-scroll/");
+}
+function openWindow1() {
+    window.open("https://andrea-everett.github.io/dark-light-mode/");
+}
+function openWindow2() {
+    window.open("https://andrea-everett.github.io/joke-teller/");
+}
+function openWindow3() {
+    window.open("https://andrea-everett.github.io/quote-generator/");
+}
+function openWindow4() {
+    window.open(" https://andrea-everett.github.io/Article-Preview/");
+}
+function openWindow5() {
+    window.open("https://andrea-everett.github.io/music-player/");
+}
