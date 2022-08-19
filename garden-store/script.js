1bd19d289fedcee9e713fbf810287599f8e40414
andrea-everett <andrea.dawn.everett@gmail.com>
2022-08-19 12:36:12 -0400

    add-repo

---
+++ garden-store/script.js
@@ -0,0 +1,19 @@
+// change nav color on hover
+function changeColor () {
+    document.querySelector('.garden-store').style.color = black ;
+}
+
+// Redirect nav pages
+function myFunction() {
+    window.open("", "_self")
+}
+function myFunction() {
+    window.open("", "_self")
+}
+function myFunction() {
+    window.open("", "_self")
+}
+function myFunction() {
+    window.open("", "_self")
+}
+
