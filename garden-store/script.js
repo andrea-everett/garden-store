7bba94f4d9ffe9fe89582df67557468d76c634a2
andrea-everett <andrea.dawn.everett@gmail.com>
2022-08-20 17:03:23 -0400

    edits

--- garden-store/script.js
+++ garden-store/script.js
@@ -1,19 +1,39 @@
-// change nav color on hover
-function changeColor () {
-    document.querySelector('.garden-store').style.color = black ;
+// change home image
+var i = 0; 
+var images = [];
+var time = 4000;
+
+images[0] = '/imgs/hero-image.jpg';
+images[1] = '/imgs/hero-image2.jpeg';
+images[2] = '/imgs/hero-image3.jpeg';
+
+function changeImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length -1 ){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
 }
 
+window.onload = changeImg;
+
+
+
 // Redirect nav pages
-function myFunction() {
-    window.open("", "_self")
-}
-function myFunction() {
-    window.open("", "_self")
-}
-function myFunction() {
-    window.open("", "_self")
-}
-function myFunction() {
-    window.open("", "_self")
-}
+// function myFunction() {
+//     window.open("", "_self")
+// }
+// function myFunction() {
+//     window.open("", "_self")
+// }
+// function myFunction() {
+//     window.open("", "_self")
+// }
+// function myFunction() {
+//     window.open("", "_self")
+// }
 
