3f8f5255768a7b3dce912095af447fcfdd0b7728
andrea-everett <andrea.dawn.everett@gmail.com>
2022-08-21 18:35:58 -0400

    fix-docs

--- docs/script.js
+++ docs/script.js
@@ -5,9 +5,9 @@ var i = 0;
 var images = [];
 var time = 4000;
 
-images[0] = '/imgs/hero-image.jpg';
-images[1] = '/imgs/hero-image2.jpeg';
-images[2] = '/imgs/hero-image3.jpeg';
+images[0] = '/docs/imgs/hero-image.jpg';
+images[1] = '/docs/imgs/hero-image2.jpeg';
+images[2] = '/docs/imgs/hero-image3.jpeg';
 
 function changeImg(){
     document.slide.src = images[i];
