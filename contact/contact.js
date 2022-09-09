08ade121d88758cff31c2c7f7c8c9368328fedb0
andrea-everett <andrea.dawn.everett@gmail.com>
2022-09-09 11:24:56 -0400

    adding-js

---
+++ contact/contact.js
@@ -0,0 +1,6 @@
+// on button click redirect to testimonials
+document.querySelector('#testimonials')
+.addEventListener('click', () => {
+    window.location.href = '/workshop/workshop.html';
+});
+
---
+++ workshop/workshop.js
@@ -0,0 +1,10 @@
+// on button click redirect to workshop info
+document.querySelector('#learn-more')
+.addEventListener('click', () => {
+    window.location.href = 'tools/tools.html';
+});
+
+document.querySelector('#learn-more')
+.addEventListener('click', () => {
+    window.location.href = 'tools/tools.html';
+});
