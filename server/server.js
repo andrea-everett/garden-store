5528d839aad50488ba142ba8e184cf6a170140c9
andrea-everett <andrea.dawn.everett@gmail.com>
2022-09-26 13:07:20 -0400

    edit-info

--- server/server.js
+++ server/server.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 
 const express = require("express")
 const app = express()
+const cors = require("cors")
 app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:5500",
+    })
+)
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
@@ -10,5 +16,32 @@ const storeItems = new Map([
     [1, { priceInCents: 10000, name: "Learn React Today"}],
     [ 2, { priceInCents: 20000, name: "Learn CSS Today"}],
 ])
+
+app.post('/create-checkout-session', async(req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            mode: 'payment',
+            line_items: req.body.items.map(item => {
+                const storeItem = storeItems.get(item.id)
+                return {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: storeItem.name
+                        },
+                        unit_amount: storeItem.priceInCents,
+                    },
+                    quantity: item.quantity
+                }
+            }),
+            success_url: `${process.env.SERVER_URL}/success.html`,
+            cancel_url: `${process.env.SERVER_URL}/cancel.html`
+        })
+        res.json({ url: 'Hi '})
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+})
 
 app.listen(3000)
