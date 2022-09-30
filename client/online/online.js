033f5f06f839746355d5460eea5a79ddddf7b714
andrea-everett <andrea.dawn.everett@gmail.com>
2022-09-30 15:14:11 -0400

    update-info

--- client/online/online.js
+++
@@ -1,5 +0,0 @@
-// on button click redirect to home page
-document.querySelector('#heading')
-.addEventListener('click', () => {
-    window.location.href = 'https://andrea-everett.github.io/garden-store//index.html';
-});
