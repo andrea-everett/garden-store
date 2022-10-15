517b6c363073beba31aab24817833d86b67c1b37
andrea-everett <andrea.dawn.everett@gmail.com>
2022-10-15 14:29:51 -0400

    edit-links

--- public/contact/contact.js
+++ public/contact/contact.js
@@ -1,12 +1,12 @@
 // on button click redirect to home page
 document.querySelector('#heading')
 .addEventListener('click', () => {
-    window.location.href = 'https://andrea-everett.github.io/garden-store/index.html';
+    window.location.href = '../index.html';
 });
 
 // on button click redirect to testimonials
 document.querySelector('#testimonials')
 .addEventListener('click', () => {
-    window.location.href = 'https://andrea-everett.github.io/garden-store/workshop/workshop.html';
+    window.location.href = '../workshop/workshop.html';
 });
 
