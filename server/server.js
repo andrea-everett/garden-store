ccb6499ea350ad997e7ea80057187ec00a5539aa
andrea-everett <andrea.dawn.everett@gmail.com>
2022-10-17 11:06:37 -0400

    edit-links

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-
 require('dotenv').config()
 
 const express = require("express")
 const app = express()
-// const path = require(path)
+const cors = require('cors')
 app.use(express.json())
-app.use(express.static('public'))
+app.use(cors({ 
+    origin: 'http://localhost:5500',
+}))
+
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 const storeItems = new Map([
     [1, { priceInCents: 10000, name: "Learn React Today"}],
     [ 2, { priceInCents: 20000, name: "Learn CSS Today"}],
 ])
-
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 const YOUR_DOMAIN = 'http://localhost:3000'
 
@@ -70,8 +72,8 @@ app.post('/create-checkout-session', async  (req, res) => {
                         quantity: item.quantity
                     }
                 }),
-                success_url: `${process.env.SERVER_URL}/success.html`,
-                cancel_url: `${process.env.SERVER_URL}/cancel.html`
+                success_url: `${process.env.PUBLIC_URL}/success.html`,
+                cancel_url: `${process.env.PUBLIC_URL}/cancel.html`
             })
             console.log('sucessfull response')
             res.status(200).json({ url: session.url })
