bfb35055cbf3717463440ef681f51aa24cd9aca5
andrea-everett <andrea.dawn.everett@gmail.com>
2022-10-31 10:50:19 -0400

    take-away-cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const path = require('path');
-const cors = require('cors')
+// const cors = require('cors')
 
 const express = require("express")
 const app = express()
@@ -8,7 +8,7 @@ const app = express()
 //     // origin: "http://localhost:5500"
 // }
 
-app.use(cors)
+// app.use(cors)
 app.use(express.static(path.join(__dirname, "..", "client")));
 app.use(express.json())
 
